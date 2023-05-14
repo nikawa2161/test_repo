45cdddd0c532c1783cf881973490fb0ccead9036
nikawa2161 <nikawa.2161.t@gmail.com>
2023-05-14 19:15:01 +0900

    サイドバーの作成

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,23 +15,29 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.admin_navigation')
+        <div class="flex">
+            {{-- サイドバー --}}
+            <aside id="default-sidebar" class="bg-gray-800 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+                @include('layouts.admin_sidebar')
+             </aside>
+            <div class="min-h-screen bg-gray-100 dark:bg-gray-900 flex-1">
+                @include('layouts.admin_navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-            <header class="bg-white dark:bg-gray-800 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                    <p>admin.blade</p>
-                </div>
-            </header>
-            @endif
+                <!-- Page Heading -->
+                @if (isset($header))
+                <header class="bg-white dark:bg-gray-800 shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                        <p>admin.blade</p>
+                    </div>
+                </header>
+                @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                <!-- Page Content -->
+                <main>
+                    {{ $slot }}
+                </main>
+            </div>
         </div>
     </body>
 </html>
