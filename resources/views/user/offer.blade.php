3911c25d3798e9b8aaed9d7030a5181b5e9e3be2
nikawa2161 <nikawa.2161.t@gmail.com>
2023-06-17 18:28:29 +0900

    一覧画面での応募ボタン表示

--- resources/views/user/offer.blade.php
+++ resources/views/user/offer.blade.php
@@ -13,15 +13,30 @@
                 <li class=" py-2 sm:py-4 lg:py-8">
                     <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
                       <div class="flex items-center justify-between rounded-lg bg-gray-100 p-2 sm:flex-row md:p-8">
-                        <div class="">
+                        <div class="w-3/4q">
                             <p class="mb-4 text-xl font-bold text-indigo-500 md:text-2xl">タイトル：{{ $offer->title }}</p>
                             <p class="text-xl font-bold text-indigo-500 md:text-2xl">内容：{{ $offer->content }}</p>
                         </div>
 
-                            <div class="">
+                            <div class="flex">
                                 <x-my-nav-link :href="route('offer.show',[ $offer->id ])">
                                     {{ __('詳細') }}
                                 </x-my-nav-link>
+                                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 flex justify-end">
+                                    @if ($entryStatus->where('offer_id', $offer->id)->exists())
+                                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                        応募済み
+                                    </button>
+                                    @else
+                                    <form method="post" action="{{ route('entry') }}">
+                                        @csrf
+                                        <input type="hidden" name="offer_id" value="{{ $offer->id }}">
+                                        <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                            応募する
+                                        </button>
+                                    </form>
+                                    @endif
+                                </div>
                             </div>
                       </div>
                     </div>
